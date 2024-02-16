68c9c8f1180759dff62759ac04f2107cc1abf288
Bruno Cardoso de Oliveira <119974445+TheBurno@users.noreply.github.com>
2024-02-16 14:13:41 -0400

    Preparação para o código, contendo os requisitos.

---
+++ codigo.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Conter Cadastro, Consulta e Exclusão de Dados
+
+// Struct
+
+// Arquivo
+
+// Funções e Procedimentos
+
+// Comandos de Entrada-Saída
+
+// Decisão
+
+// Repetição
+
+// Vetores ou Matrizes
